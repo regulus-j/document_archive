6aa4ab385fd6412247ffe49e384b9c48830c6257
ShinoX0426 <eh202201063@wmsu.edu.ph>
2025-03-19 23:54:01 +0800

    Ui Update

--- resources/views/documents/show.blade.php
+++ resources/views/documents/show.blade.php
@@ -264,11 +264,11 @@
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm">
                                         <span class="px-2 py-1 text-xs font-semibold rounded-full
-                                                                @if($log->action === 'created') bg-green-100 text-green-800
-                                                                @elseif($log->action === 'updated') bg-yellow-100 text-yellow-800
-                                                                @elseif($log->action === 'deleted') bg-red-100 text-red-800
-                                                                    @else bg-blue-100 text-blue-800
-                                                                @endif">
+                                                                                @if($log->action === 'created') bg-green-100 text-green-800
+                                                                                @elseif($log->action === 'updated') bg-yellow-100 text-yellow-800
+                                                                                @elseif($log->action === 'deleted') bg-red-100 text-red-800
+                                                                                    @else bg-blue-100 text-blue-800
+                                                                                @endif">
                                             {{ ucfirst($log->action) }}
                                         </span>
                                     </td>
