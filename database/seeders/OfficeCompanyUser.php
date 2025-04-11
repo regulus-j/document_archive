db83801df12a098ea672218ebe277359f6abc946
regulus_j <jamalalbadi03@gmail.com>
2025-04-11 10:59:32 +0800

    refactor//update OfficeCompanyUser seeder to create company accounts and assign users to offices

--- database/seeders/OfficeCompanyUser.php
+++ database/seeders/OfficeCompanyUser.php
@@ -14,16 +14,37 @@ class OfficeCompanyUser extends Seeder
      */
     public function run()
     {
-        // Create a single user
-        User::factory()->create();
-    
-        // Create 10 users
-        User::factory()->count(10)->create();
+        // Create company accounts first
+        $companies = CompanyAccount::factory()->count(2)->create();
         
-        // Create a company account for each user
-        CompanyAccount::factory()->count(10)->create();
+        // Create offices for each company
+        $offices = [];
+        foreach ($companies as $company) {
+            $companyOffices = Office::factory()
+                ->count(5)
+                ->create(['company_id' => $company->id]);
+            $offices = array_merge($offices, $companyOffices->toArray());
+        }
         
-        // Create offices
-        Office::factory()->count(10)->create();
+        // Create users and assign each to at least one office
+        User::factory()->count(11)->create()->each(function ($user) use ($offices) {
+            // Determine how many offices to assign to this user (1-3)
+            $numOffices = rand(1, 3);
+            
+            // Get random office IDs
+            $officeIds = array_column($offices, 'id');
+            shuffle($officeIds);
+            $selectedOfficeIds = array_slice($officeIds, 0, $numOffices);
+            
+            // Assign offices to user
+            $user->offices()->attach($selectedOfficeIds);
+            
+            // Optionally, assign user to a company
+            $companyId = $offices[array_search($selectedOfficeIds[0], array_column($offices, 'id'))]['company_id'];
+            \App\Models\CompanyUser::create([
+                'user_id' => $user->id,
+                'company_id' => $companyId
+            ]);
+        });
     }
 }
