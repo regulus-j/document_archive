9f762bafac19f03eadd766eb809d9cb11b1fbea2
regulus_j <your-noreply-email@users.noreply.github.com>
2025-05-12 20:34:50 +0800

    Added forward document button to docs index. Changed default value of status to uploaded, added new enum values.

--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -368,6 +368,17 @@
                                                             d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                                     </svg>
                                                 </a>
+
+                                                <a href="{{ route('documents.forward', $document->id) }}"
+                                                    class="p-1.5 bg-amber-50 text-amber-600 rounded-lg hover:bg-amber-100 transition-colors"
+                                                    title="Forward">
+                                                    <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                            d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+                                                    </svg>
+                                                </a>
+                                                
                                                 @can('document-edit')
                                                     <a href="{{ route('documents.edit', $document->id) }}"
                                                         class="p-1.5 bg-emerald-50 text-emerald-600 rounded-lg hover:bg-emerald-100 transition-colors"
