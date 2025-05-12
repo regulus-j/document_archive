64ee6fa01edf941157e6b255c4c155264b2cf9e2
ShinoX0426 <eh202201063@wmsu.edu.ph>
2025-05-13 02:04:06 +0800

    fix user create banner

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -255,7 +255,7 @@ class UserController extends Controller
         ]);
 
         return redirect()->route('users.index')
-            ->with('error', 'User created successfully');
+            ->with('success', 'User created successfully');
     }
 
     /**
