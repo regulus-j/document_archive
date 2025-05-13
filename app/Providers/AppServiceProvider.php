47b96bebe2ecc2f2f064d1531be75986849c4303
regulus_j <your-noreply-email@users.noreply.github.com>
2025-05-13 13:33:53 +0800

    Enforce HTTPS scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if(config('app.env') === 'production') {
+        \URL::forceScheme('https');
+    }
     }
 }
