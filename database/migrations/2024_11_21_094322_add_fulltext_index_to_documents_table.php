0159bcecbe5a3e4bb6d556e2821d32344724f841
regulus_j <your-noreply-email@users.noreply.github.com>
2026-01-20 06:11:45 +0800

    migrate fix

--- database/migrations/2024_11_21_094322_add_fulltext_index_to_documents_table.php
+++ database/migrations/2024_11_21_094322_add_fulltext_index_to_documents_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('documents', function (Blueprint $table) {
-            $table->fullText('content')->nullable();
+            $table->fullText('content');
         });
     }
 
